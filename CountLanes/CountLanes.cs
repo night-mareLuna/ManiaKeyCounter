ea8d951a35b86cd36242168a2e9b37a1c41809f7
night-mareLuna <lunanight115@gmail.com>
2023-10-23 22:47:38 +0200

    initial commit

---
+++ CountLanes/CountLanes.cs
@@ -0,0 +1,20 @@
+﻿namespace KeyCounter;
+public class CountLanes
+{
+	public static int[] Keys(int lanes, List<string> file)
+	{
+		// Return an int[] (the size of the key count) for the amount of keys in each lane.
+		int[] laneCount = new int[lanes];
+
+		foreach(string line in file)
+		{
+			string raw_lane_string = line.Split(',')[0];
+			int raw_lane = int.Parse(raw_lane_string);
+
+			double lane = raw_lane * lanes / 512;
+			lane = Math.Floor(Math.Clamp(lane, 0, lanes - 1));
+			laneCount[(int)lane]++;
+		}
+		return laneCount;
+	}
+}
---
+++ GetObjects/GetObjects.cs
@@ -0,0 +1,51 @@
+﻿namespace KeyCounter;
+public class GetObjects
+{
+	public static List<string> GetHitObjects(string file)
+	{
+		try
+		{
+			string[] osuFile = File.ReadAllLines(file);
+			List<string> hitObjects = new();
+			
+			bool foundObjects = false;
+			for(int i = 0; i < osuFile.Length; i++)
+			{
+				if(foundObjects) hitObjects.Add(osuFile[i]);
+				else
+				{
+					foundObjects = osuFile[i] == "[HitObjects]";
+					continue;
+				}
+			}
+			return hitObjects;
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine(e.Message);
+			return new();
+		}
+		
+	}
+
+	public static int Lanes(string file)
+	{
+		try
+		{
+			string[] osuFile = File.ReadAllLines(file);
+
+			foreach(string line in osuFile)
+			{
+				if(line.Contains("CircleSize"))
+				{
+					return (int)Char.GetNumericValue(line[^1]);
+				}
+			}
+		}
+		catch (IOException e)
+		{
+			Console.WriteLine(e.Message);
+		}
+		return 0;
+	}
+}
