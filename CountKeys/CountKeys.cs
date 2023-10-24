73b50a4be4a757e4e0b0b5f9a037c404fe4c2c0f
night-mareLuna <lunanight115@gmail.com>
2023-10-24 23:12:09 +0200

    changes to acommodate for avalonia file selector

--- CountKeys/CountKeys.cs
+++ CountKeys/CountKeys.cs
@@ -9,11 +9,12 @@ public class CountKeys
 		foreach(string line in file)
 		{
 			string raw_lane_string = line.Split(',')[0];
-			int raw_lane = int.Parse(raw_lane_string);
-
-			double lane = raw_lane * lanes / 512;
-			lane = Math.Floor(Math.Clamp(lane, 0, lanes - 1));
-			laneCount[(int)lane]++;
+			if (int.TryParse(raw_lane_string, out int raw_lane))
+			{
+				double lane = raw_lane * lanes / 512;
+				lane = Math.Floor(Math.Clamp(lane, 0, lanes - 1));
+				laneCount[(int)lane]++;
+			}
 		}
 		return laneCount;
 	}
