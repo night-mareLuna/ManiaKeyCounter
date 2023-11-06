d0de3069464b72883eeb21f80fac91228a236f3f
night-mareLuna <lunanight115@gmail.com>
2023-11-07 01:25:00 +0200

    add 2nd button event and fixed program size

--- AvaloniaUI/MainWindow.axaml.cs
+++ AvaloniaUI/MainWindow.axaml.cs
@@ -8,13 +8,44 @@ namespace KeyCounter
 	    public MainWindow()
 	    {
 	        InitializeComponent();
+			Width = 600;
+			Height = 400;
+
+			MinWidth = 600;
+			MinHeight = 400;
+
+			MaxWidth = 600;
+			MaxHeight = 400;
 	    }
 
-		public async void OpenFile(object source, RoutedEventArgs args)
+		public void OpenFile1(object source, RoutedEventArgs args)
+		{
+			OpenFile(1);
+		}
+
+		public void OpenFile2(object source, RoutedEventArgs args)
+		{
+			OpenFile(2);
+		}
+
+		private async void OpenFile(int button)
 		{
 			var storage = StorageProvider;
 			string[]? osuFile = await BeatmapPicker.GetOsuBeatmap(storage);
-			key_counter.Text = GetObjects.IsOsuManiaFile(osuFile!) ? OutputText.GetDisplayText(osuFile!) : "This is not an osu!mania file.";
+			if(osuFile is null) return;
+			
+			switch(button)
+			{
+				case 1:
+					key_counter_1.Text = GetObjects.IsOsuManiaFile(osuFile!) ? OutputText.GetDisplayText(osuFile!, true) : "This is not an osu!mania file.";
+					break;
+				case 2:
+					key_counter_2.Text = GetObjects.IsOsuManiaFile(osuFile!) ? OutputText.GetDisplayText(osuFile!, true) : "This is not an osu!mania file.";
+					break;
+				default:
+					break;
+			}
+			
 		}
 	}
 }
